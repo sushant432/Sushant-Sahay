ac1022ec548c2c44410fa0d287b0d52f84ebee4e
sushant432 <69951656+sushant432@users.noreply.github.com>
2020-10-20 13:37:31 +0530

    Add files via upload

---
+++ Assignment.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Assignment.aspx.cs" Inherits="WebApplication4.Assignment" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<script src="JavaScript1.js"></script>
+    
+<body>
+    <form id="form1" runat="server">
+    <div>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ JavaScript1.js
@@ -0,0 +1,23 @@
+﻿function FindPair(numofsocks,array)
+{
+    var counter=0;
+    for(var i=0;i<array.length;i++)
+    {
+    var countValue = 1;
+    for(var j=i+1;j<array.length;j++)
+    {
+    if(array[i]==array[j])
+    {
+       countValue++;
+    }
+    }
+    if (countValue % 2 == 0)
+    {
+    counter++;
+    }
+    }
+    return counter;
+    
+}
+var result = FindPair(17,[1, 1, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5 ,6, 6, 7, 7, 7])
+alert(result);
